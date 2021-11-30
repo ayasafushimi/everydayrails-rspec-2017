c69c136f504956a4685713ceab618e0fe4602804
ayasafushimi <bolbus0617@gmail.com>
2021-11-30 18:21:00 +0900

    add FactoryBot

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,13 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Project, type: :model do
   before do
-    @user = User.create(
-      first_name: "Joe",
-      last_name: "Tester",
-      email: "joetester@example.com",
-      password: "dottle-nouveau-pavilion-tights-furze",
-      )
+    @user = FactoryBot.create(:user)
   end
+
   # 名前、ユーザーがあれば有効な状態であること
   it "is valid with a name and user" do
     project = Project.new(
@@ -17,6 +13,7 @@ RSpec.describe Project, type: :model do
     )
     expect(project).to be_valid
   end
+
   # 名前がなければ無効な状態であること
   it "is invalid without a name" do
     project = Project.new(
@@ -25,6 +22,7 @@ RSpec.describe Project, type: :model do
     project.valid?
     expect(project.errors[:name]).to include("can't be blank")
   end
+
   # ユーザー単位では重複したプロジェクト名を許可しないこと
   it "does not allow duplicate project names per user" do
     @user.projects.create(
@@ -38,25 +36,26 @@ RSpec.describe Project, type: :model do
     new_project.valid?
     expect(new_project.errors[:name]).to include("has already been taken")
   end
+
   # 二人のユーザーが同じ名前を使うことは許可すること
   it "allows two users to share a project name" do
-    user = User.create(
+    user = FactoryBot.create(
+      :user,
       first_name: "Joe",
-      last_name: "Tester",
       email: "tester@example.com",
-      password: "dottele-nouveau-pavilion-tights-furze",
-    )
+      )
+    p user
 
     user.projects.create(
       name: "Test Project",
     )
 
-    other_user = User.create(
+    other_user = FactoryBot.create(
+      :user,
       first_name: "Jane",
-      last_name: "Tester",
       email: "janetester@example.com",
-      password: "dottle-nouveau-pavilion-tights-furze",
     )
+    p other_user
 
     other_project = other_user.projects.build(
       name: "Test Project",
